bc7610cd042171ab62e8b596484ba78a370a8f15
brettlangdon <brett@blangdon.com>
2015-03-08 16:58:20 -0400

    update main.js

--- receiver/main.js
+++ receiver/main.js
@@ -2,56 +2,61 @@
  * Main JavaScript for handling Chromecast interactions.
  */
 
+var urls = [];
+var index = 0;
+var delay = 30;
+var interval = null;
+
+
 window.onload = function() {
-  cast.receiver.logger.setLevelValue(0);
-  window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-  console.log('Starting Receiver Manager');
+    cast.receiver.logger.setLevelValue(0);
+    window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+    console.log('Starting Receiver Manager');
 
-  castReceiverManager.onReady = function(event) {
-    console.log('Received Ready event: ' + JSON.stringify(event.data));
-    window.castReceiverManager.setApplicationState('chromecast-dashboard is ready...');
-  };
+    castReceiverManager.onReady = function(event) {
+        console.log('Received Ready event: ' + JSON.stringify(event.data));
+        window.castReceiverManager.setApplicationState('chromecast-dashboard is ready...');
+    };
 
-  castReceiverManager.onSenderConnected = function(event) {
-    console.log('Received Sender Connected event: ' + event.senderId);
-  };
+    castReceiverManager.onSenderConnected = function(event) {
+        console.log('Received Sender Connected event: ' + event.senderId);
+    };
 
-  castReceiverManager.onSenderDisconnected = function(event) {
-    console.log('Received Sender Disconnected event: ' + event.senderId);
-  };
+    castReceiverManager.onSenderDisconnected = function(event) {
+        console.log('Received Sender Disconnected event: ' + event.senderId);
+    };
 
-  window.messageBus =
-    window.castReceiverManager.getCastMessageBus(
-        'urn:x-cast:com.boombatower.chromecast-dashboard', cast.receiver.CastMessageBus.MessageType.JSON);
+    window.messageBus =
+        window.castReceiverManager.getCastMessageBus(
+            'urn:x-cast:is.brett.chromecast-dashboard', cast.receiver.CastMessageBus.MessageType.JSON);
 
-  window.messageBus.onMessage = function(event) {
-    console.log('Message [' + event.senderId + ']: ' + event.data);
+    window.messageBus.onMessage = function(event) {
+        console.log('Message [' + event.senderId + ']: ' + event.data);
 
-    if (event.data['type'] == 'load') {
-      $('#dashboard').attr('src', event.data['url']);
-      if (event.data['refresh'] > 0) {
-        $('#dashboard').attr('data-refresh', event.data['refresh'] * 1000);
-        setTimeout(reloadDashboard, $('#dashboard').attr('data-refresh'));
-      }
-      else {
-        $('#dashboard').attr('data-refresh', 0);
-      }
+        if (event.data.type === 'load') {
+            urls = event.data.urls;
+            delay = event.data.delay;
+            clearInterval(interval);
+            interval = setInterval(function(){
+                if(!urls.length){
+                    return;
+                }
+                index += 1;
+                if(index > urls.length){
+                    index = 0;
+                }
+
+                $('#dashboard').attr('src', urls[index]);
+            }, delay);
+        }
     }
-  }
 
-  // Initialize the CastReceiverManager with an application status message.
-  window.castReceiverManager.start({statusText: 'Application is starting'});
-  console.log('Receiver Manager started');
+    // Initialize the CastReceiverManager with an application status message.
+    window.castReceiverManager.start({statusText: 'Application is starting'});
+    console.log('Receiver Manager started');
 
-  function reloadDashboard() {
-    $('#dashboard').attr('src', $('#dashboard').attr('src'));
-    if ($('#dashboard').attr('data-refresh')) {
-      setTimeout(reloadDashboard, $('#dashboard').attr('data-refresh'));
-    }
-  }
-
-  $('#dashboard').load(function() {
-    $('#loading').hide();
-    console.log('Loading animation hidden.');
-  });
+    $('#dashboard').load(function() {
+        $('#center').hide();
+        console.log('Loading animation hidden.');
+    });
 };
